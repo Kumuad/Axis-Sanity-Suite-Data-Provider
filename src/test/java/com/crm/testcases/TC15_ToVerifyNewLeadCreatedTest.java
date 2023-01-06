6c6f8d0c8f554d8c3906128d1d7cca59e226f30e
kumuad <kumuad.sagar@crmnext.com>
2023-01-06 20:38:23 +0530

    testcase updated

--- src/test/java/com/crm/testcases/TC15_ToVerifyNewLeadCreatedTest.java
+++ src/test/java/com/crm/testcases/TC15_ToVerifyNewLeadCreatedTest.java
@@ -4,9 +4,7 @@ import java.util.ArrayList;
 import java.util.Hashtable;
 
 import org.testng.SkipException;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Listeners;
+
 import org.testng.annotations.Test;
 
 import com.crm.base.TestBase;
@@ -17,7 +15,7 @@ import com.crm.pages.RetailAppointmentPage;
 import com.crm.pages.TaskPage;
 import com.crm.utilities.CommonMethods;
 import com.crm.utilities.ScreenShot;
-import com.crm.listeners.CustomListener;
+
 
 public class TC15_ToVerifyNewLeadCreatedTest extends TestBase {
 
